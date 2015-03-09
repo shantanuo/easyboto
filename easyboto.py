9ea79840b0ddcf4099fee94688c695864d7aed8b
Shantanu Oak <shantanu.oak@gmail.com>
2015-03-09 17:01:53 +0530

    Update easyboto.py
    updated indents

--- easyboto.py
+++ easyboto.py
@@ -8,48 +8,37 @@ class myboto:
     def __init__(self, access, secret):
         self.ac = access
         self.se = secret
-        self.red_conn = boto.connect_redshift(aws_access_key_id=self.ac, 
-aws_secret_access_key=self.se)
+        self.red_conn = boto.connect_redshift(aws_access_key_id=self.ac, aws_secret_access_key=self.se)
         
     def _showSnap(self):
         self.mydict=self.red_conn.describe_clusters()
-        
-self.my_add=self.mydict['DescribeClustersResponse']['DescribeClustersResult']['Clusters'][0]['Endpoint']['Address']
+        self.my_add=self.mydict['DescribeClustersResponse']['DescribeClustersResult']['Clusters'][0]['Endpoint']['Address']
         self.response = self.red_conn.describe_cluster_snapshots()
-        self.snapshots = 
-self.response['DescribeClusterSnapshotsResponse']['DescribeClusterSnapshotsResult']['Snapshots']
+        self.snapshots = self.response['DescribeClusterSnapshotsResponse']['DescribeClusterSnapshotsResult']['Snapshots']
         self.snapshots.sort(key=lambda d: d['SnapshotCreateTime'])
         self.mysnapidentifier = self.snapshots[-1]['SnapshotIdentifier']
         self.df=pd.DataFrame(self.snapshots)
-        self.df['ClusterCreateDate'] = 
-pd.to_datetime(self.df['ClusterCreateTime'],unit='s')
-        self.df['SnapshotCreateDate'] = 
-pd.to_datetime(self.df['SnapshotCreateTime'],unit='s')
+        self.df['ClusterCreateDate'] = pd.to_datetime(self.df['ClusterCreateTime'],unit='s')
+        self.df['SnapshotCreateDate'] = pd.to_datetime(self.df['SnapshotCreateTime'],unit='s')
         self.df['BackupSize'] = self.df['TotalBackupSizeInMegaBytes']
-        self.df['IncrementSize'] = 
-self.df['ActualIncrementalBackupSizeInMegaBytes']
-        self.newdf=self.df[['SnapshotIdentifier','SnapshotCreateDate', 
-'BackupSize', 'ClusterIdentifier', 'ClusterCreateDate', 'IncrementSize', 
-'SnapshotType'] ]
+        self.df['IncrementSize'] = self.df['ActualIncrementalBackupSizeInMegaBytes']
+        self.newdf=self.df[['SnapshotIdentifier','SnapshotCreateDate', 'BackupSize', 'ClusterIdentifier', 'ClusterCreateDate', 'IncrementSize', 'SnapshotType'] ]
 
 ##### cluster management #####
 
     def showCluster(self):
         ''' show details of the cluster currently active '''
         self._showSnap()
-        
-pprint(self.mydict['DescribeClustersResponse']['DescribeClustersResult']['Clusters'])
+        pprint(self.mydict['DescribeClustersResponse']['DescribeClustersResult']['Clusters'])
         
     def startCluster(self):
         '''start a new cluster if no cluster is active'''
         self._showSnap()
         try:
-            
-myidentifier=self.mydict['DescribeClustersResponse']['DescribeClustersResult']['Clusters'][0]['ClusterIdentifier']
+            myidentifier=self.mydict['DescribeClustersResponse']['DescribeClustersResult']['Clusters'][0]['ClusterIdentifier']
             print "cluster already running"
         except IndexError:
-            self.red_conn.restore_from_cluster_snapshot('company', 
-mysnapidentifier, availability_zone='us-east-1a')
+            self.red_conn.restore_from_cluster_snapshot('company', mysnapidentifier, availability_zone='us-east-1a')
 
     def deleteCluster(self):
         import datetime
@@ -57,12 +46,9 @@ mysnapidentifier, availability_zone='us-east-1a')
         myday = datetime.datetime.now().strftime("%d")
         self.myvar = mymonth+myday+'company-new'
         mydict=self.red_conn.describe_clusters()
-        
-self.myidentifier=mydict['DescribeClustersResponse']['DescribeClustersResult']['Clusters'][0]['ClusterIdentifier']
-        print "this does not actually delete the cluster. Run the following 
-command on your own risk :)\n"
-        print "conn.delete_cluster(%s, skip_final_cluster_snapshot=False, 
-final_cluster_snapshot_identifier=%s)"%(self.myidentifier, self.myvar)
+        self.myidentifier=mydict['DescribeClustersResponse']['DescribeClustersResult']['Clusters'][0]['ClusterIdentifier']
+        print "this does not actually delete the cluster. Run the following command on your own risk :)\n"
+        print "conn.delete_cluster(%s, skip_final_cluster_snapshot=False, final_cluster_snapshot_identifier=%s)"%(self.myidentifier, self.myvar)
         
 ##### Snapshots Management #####
 
@@ -73,14 +59,12 @@ final_cluster_snapshot_identifier=%s)"%(self.myidentifier, self.myvar)
 
     def snapshotCreate(self):
         pass
-        # red_conn.create_cluster_snapshot(self, snapshot_identifier, 
-cluster_identifier)
+        # red_conn.create_cluster_snapshot(self, snapshot_identifier, cluster_identifier)
         
     def snapshotDelete(self):
         pass
 
-    def snapshotDown(self, 
-path='/var/www/html/new_email360_panel/abcx.csv'):
+    def snapshotDown(self, path='/var/www/html/new_email360_panel/abcx.csv'):
         '''copy snapshot data to csv that can be downloaded from the path'''
         self._showSnap()
         self.newdf.to_csv(path)
@@ -90,8 +74,7 @@ path='/var/www/html/new_email360_panel/abcx.csv'):
 
     def runQuery(self, my_query):
         self._showSnap()
-        pconn = psycopg2.connect("host='"+self.my_add+"' port='5439' 
-dbname='mydb' user='root' password='Root1234'")
+        pconn = psycopg2.connect("host='"+self.my_add+"' port='5439' dbname='mydb' user='root' password='Root1234'")
         cur = pconn.cursor()            
         cur.execute(my_query)
         mydict = cur.fetchall()
