e0fb48583743e9ebd2474f7c91ba39c59547474d
Shantanu Oak <shantanu.oak@gmail.com>
2016-06-02 18:43:43 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'easyboto',
+    packages = ['easyboto'],
+    version = '0.8',  # Ideally should be same as your github release tag varsion
+    description = 'description',
+    author = 'Shantanu Oak',
+    author_email = 'shantanu.oak@gmail.com',
+    url = 'https://github.com/shantanuo/easyboto',
+    download_url = 'https://github.com/shantanuo/easyboto/archive/0.8.tar.gz',
+    keywords = ['aws', 'boto'],
+    classifiers = [],
+)
