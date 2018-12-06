b9307d3fed0484efaca8b92d6710b1b648ceaf52
Shantanu Oak <shantanu.oak@gmail.com>
2018-12-06 10:43:20 +0000

    Update aws_command_line.sh

--- aws_command_line.sh
+++ aws_command_line.sh
@@ -1,9 +1,21 @@
-# vi specification.json
+#!/bin/sh
+cat > userdata.sh << "here_doc"
+#!/bin/bash -ex
+yum install -y docker mysql git python-pip
+sudo amazon-linux-extras install -y docker
+pip install aws-ec2-assign-elastic-ip
+aws-ec2-assign-elastic-ip --access-key {}, --secret-key {}  --valid-ips {}
+service docker start
+docker run -d -p 8887:8888 -v /tmp:/tmp shantanuo/notebook
+here_doc
+
+
+cat > specification.json << "here_doc"
     {
       "ImageId": "ami-009d6802948d06e52",
       "InstanceType": "c4.large",
       "SubnetId": "subnet-8bb550d1",
-      "KeyName": "dec12a",
+      "KeyName": "dec15a",
       "BlockDeviceMappings": [
         {
           "DeviceName": "/dev/xvda",
@@ -13,10 +25,15 @@
             "VolumeSize": 500
           }
         }
-      ]
+      ],
+
+here_doc
+
+echo '"UserData": "'`base64 -w 0 userdata.sh`'"'>> specification.json
+
+cat >> specification.json << "here_doc"
     }
 
-
-# aws ec2 request-spot-instances --spot-price "1.050" --instance-count 1 --type "one-time" --launch-specification file://specification.json
-# "UserData":"`base64 -w 0 userdata.sh`",
-# userdata should be replaced by the actual output of base64 command
+here_doc
+ 
+aws ec2 request-spot-instances --spot-price "1.050" --instance-count 1 --type "one-time" --launch-specification file://specification.json
